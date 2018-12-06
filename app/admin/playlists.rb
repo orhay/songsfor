cfdaf0ae2225ec1921cae658096e91d201dff4a1
Raghu Betina <raghu@firstdraft.com>
2018-12-06 01:51:20 +0000

    Whitelist columns in Playlist for ActiveAdmin

--- app/admin/playlists.rb
+++ app/admin/playlists.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Playlist do
+
+ permit_params :description, :user_id, :playlist_name, :spotify_playlist_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
