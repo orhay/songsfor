25698dbc6136758be6b5aadf1b4001a82323585b
Raghu Betina <raghu@firstdraft.com>
2018-12-06 01:51:10 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "playlists#index"
   # Routes for the Playlists song resource:
 
