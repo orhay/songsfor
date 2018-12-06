1b1bfd768ff8f209070cee115f3f3bddc0ae4614
Raghu Betina <raghu@firstdraft.com>
2018-12-06 01:52:58 +0000

    Only playlist user can modify playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,4 +1,14 @@
 class PlaylistsController < ApplicationController
+  before_action :current_user_must_be_playlist_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_playlist_user
+    playlist = Playlist.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == playlist.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @playlists = Playlist.all
 
